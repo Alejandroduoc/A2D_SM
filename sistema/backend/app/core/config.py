5975d707fe7995d9f1f13c47370caab77ff3020f
Alejandroduoc <136765642+Alejandroduoc@users.noreply.github.com>
2026-09-07 14:27:03 -0300

    avance del config de PY

--- sistema/backend/app/core/config.py
+++ sistema/backend/app/core/config.py
@@ -10,4 +10,19 @@ class Settings(BaseSettings):
         env_file=".env",  # indica que se debe leer el archivo .env en la raíz del proyecto para obtener las variables de entorno.
         extra="ignore" ) # indica que si hay variables de entorno que no están declaradas en la clase, se ignoren y no generen error.
 
-database_url: str="mssql+pyodbc://sa:CAMBIAR_EN_.env@localhost:1433/socradex?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
+    database_url: str="mssql+pyodbc://sa:CAMBIAR_EN_.env@localhost:1433/socradex?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
+    #Un atributo con type hint (str) y un valor por defecto. Pydantic busca DATABASE_URL en el entorno; si no la encuentra, usa ese string.
+
+    #seguridad 
+    secret_key: str="CAMBIAR_EN_.env_NUNCA_USAR_ESTE_VALOR_EN_PRODUCCION"# Un atributo con type hint (str) y un valor por defecto. Pydantic busca SECRET_KEY en el entorno; si no la encuentra, usa ese string.
+    algorithm: str="HS256" # Un atributo con type hint (str) y un valor por defecto. Pydantic busca ALGORITHM en el entorno; si no la encuentra, usa ese string.
+
+
+    #cors
+    cors_origins: list[str]=["http://localhost:5173"] # direcciones permitidas
+@lru_cache
+def get_settings() -> Settings:
+    #función que devuelve una instancia de Settings. Al usar lru_cache, la primera vez que se llama, se crea la instancia y se guarda en memoria; las siguientes veces, se devuelve la misma instancia sin recalcularla.
+    return Settings()   
+settings = get_settings() # se llama a la función y se guarda la instancia en la variable settings. Esta variable se puede importar en otros módulos para acceder a la configuración.   
+
